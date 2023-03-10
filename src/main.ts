305ba432969d2c684baf153cb33131fa4af56f36
Victor Kayk <victorkayk77@gmail.com>
2023-03-10 01:01:50 -0300

    refactor: remove dockerfile

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(passport.initialize());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
